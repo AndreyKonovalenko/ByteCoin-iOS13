4a74a70170102b27c2391c8615fe1af5ed2c6c18
AndreyKonovalenko <andreinew@mail.ru>
2020-11-26 08:09:53 +0300

    feat: add gitignore move coinapi.io apikey in separate file

---
+++ ByteCoin/key.swift
@@ -0,0 +1,13 @@
+//
+//  key.swift
+//  ByteCoin
+//
+//  Created by Indradev on 26.11.2020.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let key = "D6A3948D-7D78-431E-920B-B569EF17F04D"
+}
